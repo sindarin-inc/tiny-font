6005c3f49b8c78793d98704805afbef20dd3820f
Ben Chelf <echelf@gmail.com>
2023-07-25 21:03:31 -0400

    remove all non Sol Sans fonts (#655)
    Co-authored-by: Ben Chelf <bchelf@Bens-MacBook-Air.local>

--- src/Fonts.cpp
+++ src/Fonts.cpp
@@ -8,25 +8,9 @@
 #include "FontEntry.hpp"
 #include "IBMFDriver/IBMFFont.hpp"
 #include "IBMFDriver/IBMFFontLow.hpp"
-#include "IBMFFonts/Arial_75.h"
-#include "IBMFFonts/Baskerville_75.h"
-#include "IBMFFonts/BeausiteClassic_75.h"
-#include "IBMFFonts/ComicSans_75.h"
-#include "IBMFFonts/DejaVuSansCondensed_75.h"
-#include "IBMFFonts/DejaVuSans_75.h"
-#include "IBMFFonts/FiraSans_75.h"
-#include "IBMFFonts/FreeSans_75.h"
-#include "IBMFFonts/GnuUnifont_75.h"
-#include "IBMFFonts/Liberation_75.h"
-#include "IBMFFonts/NotoSansLight_75.h"
-#include "IBMFFonts/NotoSansMedium_75.h"
-#include "IBMFFonts/NotoSans_75.h"
-#include "IBMFFonts/OpenSansCondensed_75.h"
-#include "IBMFFonts/OpenSans_75.h"
-#include "IBMFFonts/RobotoCondensed_75.h"
-#include "IBMFFonts/Roboto_75.h"
 #include "IBMFFonts/SolSans_75.h"
-#include "IBMFFonts/Tahoma_75.h"
+
+// clang-format on
 
 // Here is the list of all fonts that can be selected through a menu in the application.
 //
@@ -45,28 +29,9 @@
 #define FONT_ENTRY(caption, name) {caption, name##_75_IBMF, name##_75_IBMF_LEN},
 
 // clang-format off
-std::array<FontEntry, 19> fontsList{{
+std::array<FontEntry, 1> fontsList{{
     FONT_ENTRY("SolSans", SOLSANS)
-    FONT_ENTRY("GnuUnifont", GNUUNIFONT)
-    FONT_ENTRY("Tahoma", TAHOMA)
-    FONT_ENTRY("Arial", ARIAL)
-    FONT_ENTRY("Baskerville", BASKERVILLE)
-    FONT_ENTRY("Beausite", BEAUSITECLASSIC)
-    FONT_ENTRY("Comic", COMICSANS)
-    FONT_ENTRY("DejaVu", DEJAVUSANS)
-    FONT_ENTRY("DejaVuCondensed", DEJAVUSANSCONDENSED)
-    FONT_ENTRY("Fira", FIRASANS)
-    FONT_ENTRY("Free", FREESANS)
-    FONT_ENTRY("Liberation", LIBERATION)
-    FONT_ENTRY("Noto", NOTOSANS)
-    FONT_ENTRY("NotoLight", NOTOSANSLIGHT)
-    FONT_ENTRY("NotoMedium", NOTOSANSMEDIUM)
-    FONT_ENTRY("Open", OPENSANS)
-    FONT_ENTRY("OpenCondensed", OPENSANSCONDENSED)
-    FONT_ENTRY("Roboto", ROBOTO)
-    FONT_ENTRY("RobotoCondensed", ROBOTOCONDENSED)
 }};
-// clang-format on
 
 // The following defines the instance of a font that is containing all faces related to that font.
 // The parameters are used to initialize the font at boot time to one of the fonts that is part of
@@ -86,31 +51,3 @@ IBMFFont ibmf75Face0 = IBMFFont(ibmf75, 0);
 IBMFFont ibmf75Face1 = IBMFFont(ibmf75, 1);
 IBMFFont ibmf75Face2 = IBMFFont(ibmf75, 2);
 IBMFFont ibmf75Face3 = IBMFFont(ibmf75, 3);
-
-// ----- GFX Fonts -----
-
-#include "GFXFonts/LucidaGrande10pt7b.h"
-#include "GFXFonts/LucidaGrande14pt7b.h"
-#include "GFXFonts/LucidaGrande5pt7b.h"
-#include "GFXFonts/LucidaGrande6pt7b.h"
-#include "GFXFonts/Tahoma10pt7b.h"
-#include "GFXFonts/Tahoma20pt7b.h"
-#include "GFXFonts/Tahoma5pt7b.h"
-#include "GFXFonts/Tahoma6pt7b.h"
-#include "GFXFonts/Tahoma7pt7b.h"
-#include "GFXFonts/Tahoma8pt7b.h"
-#include "GFXFonts/Tahoma9pt7b.h"
-
-GFont fontLucidaGrandE5PT7B = GFont(LUCIDA_GRANDE5PT7B);
-GFont fontLucidaGrandE5PT7BSkinny = GFont(LUCIDA_GRANDE5PT7B_SKINNY);
-GFont fontLucidaGrandE5PT7BFat = GFont(LUCIDA_GRANDE5PT7B_FAT);
-GFont fontLucidaGrandE6PT7B = GFont(LUCIDA_GRANDE6PT7B);
-GFont fontLucidaGrandE10PT7B = GFont(LUCIDA_GRANDE10PT7B);
-GFont fontLucidaGrandE14PT7B = GFont(LUCIDA_GRANDE14PT7B);
-GFont fontTahomaRegularFonT5PT7B = GFont(TAHOMA_REGULAR_FONT5PT7B);
-GFont fontTahomaRegularFonT6PT7B = GFont(TAHOMA_REGULAR_FONT6PT7B);
-GFont fontTahomaRegularFonT7PT7B = GFont(TAHOMA_REGULAR_FONT7PT7B);
-GFont fontTahomaRegularFonT8PT7B = GFont(TAHOMA_REGULAR_FONT8PT7B);
-GFont fontTahomaRegularFonT9PT7B = GFont(TAHOMA_REGULAR_FONT9PT7B);
-GFont fontTahomaRegularFonT10PT7B = GFont(TAHOMA_REGULAR_FONT10PT7B);
-GFont fontTahomaRegularFonT20PT7B = GFont(TAHOMA_REGULAR_FONT10PT7B);
--- src/Fonts.hpp
+++ src/Fonts.hpp
@@ -11,25 +11,11 @@
 
 // All the fonts are instanciated in Fonts.cpp
 
+extern std::array<FontEntry, 1> fontsList;
+
 extern IBMFFontLow ibmf75;
 
 extern IBMFFont ibmf75Face0;
 extern IBMFFont ibmf75Face1;
 extern IBMFFont ibmf75Face2;
 extern IBMFFont ibmf75Face3;
-
-extern std::array<FontEntry, 18> fontsList;
-
-extern GFont fontLucidaGrandE5PT7B;
-extern GFont fontLucidaGrandE5PT7BSkinny;
-extern GFont fontLucidaGrandE5PT7BFat;
-extern GFont fontLucidaGrandE6PT7B;
-extern GFont fontLucidaGrandE10PT7B;
-extern GFont fontLucidaGrandE14PT7B;
-extern GFont fontTahomaRegularFonT5PT7B;
-extern GFont fontTahomaRegularFonT6PT7B;
-extern GFont fontTahomaRegularFonT7PT7B;
-extern GFont fontTahomaRegularFonT8PT7B;
-extern GFont fontTahomaRegularFonT9PT7B;
-extern GFont fontTahomaRegularFonT10PT7B;
-extern GFont fontTahomaRegularFonT20PT7B;
